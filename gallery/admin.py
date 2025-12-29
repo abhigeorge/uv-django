5149bf4c90731592a8ab662f6be5128ca7ad875c
abhigeorge <abhigeorgekalladayil@gmail.com>
2025-12-29 06:16:12 +0530

    image preview added

--- gallery/admin.py
+++ gallery/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from django.utils.html import format_html
+from .models import Image
 
-# Register your models here.
+@admin.register(Image)
+class ImageAdmin(admin.ModelAdmin):
+    list_display = ("title", "uploaded_at", "image_preview")
+    list_filter = ("uploaded_at",)
+    search_fields = ("title",)
+
+    def image_preview(self, obj):
+        if obj.image:
+            return format_html('<img src="{}" width="100" />', obj.image.url)
+        return "No Image"
+    image_preview.short_description = "Preview"
