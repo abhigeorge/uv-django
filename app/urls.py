bc7796be1c45277703bdcce1f9a25aaad89d041e
abhigeorge <abhigeorgekalladayil@gmail.com>
2025-12-29 06:24:01 +0530

    route updated

--- app/urls.py
+++ app/urls.py
@@ -6,7 +6,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("gallery/", include("gallery.urls")),
+    path("", include("gallery.urls")),
 ]
 
 if settings.DEBUG:
